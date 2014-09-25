9b6f0b3c44cbff99fa64ca3886d54784f6cc2ade
Andrea Zoli <zoli@iasfbo.inaf.it>
2014-09-25 15:27:31 +0200

    Add fullscreen support for videos.

--- app/src/main/java/it/inaf/android/VideoDetailFragment.java
+++ app/src/main/java/it/inaf/android/VideoDetailFragment.java
@@ -4,14 +4,18 @@
 
 package it.inaf.android;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -22,6 +26,8 @@ public class VideoDetailFragment extends Fragment {
     private String mVideoUrl;
     private String mvisCounter;
     private String mDescription;
+    private VideoEnabledWebView mWebView;
+    private VideoEnabledWebChromeClient mWebChromeClient;
 
     public static final String ARG_ITEM_ID = "item_id";
 
@@ -45,11 +51,57 @@ public class VideoDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ScrollView scroll = (ScrollView) inflater.inflate(R.layout.fragment_video_detail, null);
-        TextView title = (TextView) scroll.findViewById(R.id.video_detail_title);
-        TextView date = (TextView) scroll.findViewById(R.id.video_detail_date);
-        WebView video = (WebView) scroll.findViewById(R.id.video_detail_video);
+        RelativeLayout root = (RelativeLayout) inflater.inflate(R.layout.fragment_video_detail, null);
+        ScrollView scroll = (ScrollView) root.findViewById(R.id.video_detail_container);
+        TextView title = (TextView) root.findViewById(R.id.video_detail_title);
+        TextView date = (TextView) root.findViewById(R.id.video_detail_date);
+        WebView video = (WebView) root.findViewById(R.id.video_detail_video);
+        RelativeLayout videoLayout = (RelativeLayout) root.findViewById(R.id.video_detail_fs_layout);
+        View progress = root.findViewById(R.id.video_detail_loading);
         video.getSettings().setJavaScriptEnabled(true);
+
+        mWebChromeClient = new VideoEnabledWebChromeClient(scroll, videoLayout, progress, (VideoEnabledWebView) video)
+        {
+            @Override
+            public void onProgressChanged(WebView view, int progress)
+            {
+            }
+        };
+
+        mWebChromeClient.setOnToggledFullscreen(new VideoEnabledWebChromeClient.ToggledFullscreenCallback()
+        {
+            @Override
+            public void toggledFullscreen(boolean fullscreen)
+            {
+                Window w = getActivity().getWindow();
+                if (fullscreen)
+                {
+                    // TODO remove actionbar
+                    WindowManager.LayoutParams attrs = w.getAttributes();
+                    attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+                    attrs.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                    w.setAttributes(attrs);
+                    if (android.os.Build.VERSION.SDK_INT >= 14)
+                    {
+                        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+                    }
+                }
+                else
+                {
+                    // TODO add actionbar
+                    WindowManager.LayoutParams attrs = w.getAttributes();
+                    attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
+                    attrs.flags &= ~WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+                    w.setAttributes(attrs);
+                    if (android.os.Build.VERSION.SDK_INT >= 14)
+                    {
+                        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+                    }
+                }
+            }
+        });
+
+        video.setWebChromeClient(mWebChromeClient);
         video.setPadding(0, 0, 0, 0);
         DisplayMetrics displayMetrics = getActivity().getResources().getDisplayMetrics();
         int width = displayMetrics.widthPixels;
@@ -64,7 +116,7 @@ public class VideoDetailFragment extends Fragment {
         visualCounter.setText("Visualizzazioni: "+mvisCounter);
         description.setText(mDescription);
 
-        return scroll;
+        return root;
     }
 
     @Override
@@ -81,7 +133,20 @@ public class VideoDetailFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
+
         WebView contentView = (WebView) getActivity().findViewById(R.id.video_detail_video);
         contentView.onPause();
+    }
+
+    public boolean onBackPressed() {
+        if (!mWebChromeClient.onBackPressed()) {
+            if (mWebView.canGoBack()) {
+                mWebView.goBack();
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
